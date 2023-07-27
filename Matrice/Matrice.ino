ffa86eabda8ba089804b298bc5025425d1307aed
Benjamin Gaertner <benjaming@outlook.fr>
2023-07-27 09:44:28 +0200

    durqtion updte

--- Matrice/Matrice.ino
+++ Matrice/Matrice.ino
@@ -14,17 +14,16 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
 int x = matrix.width();
 int y = 0;
 
-const char *textToPrint = "Info: Reunion mercredi 14h14 en cantina        RagnaMod VI: IP sur Discord";
+const char *textToPrint = "Info: Reunion mercredi 14h14 en cantina        Serveur Minecraft: IP sur Discord";
 const int scrollSpeed = 30;
 int textWidth;
 
-const unsigned long executionDuration = 8 * 60 *60000; // 8hours
-const unsigned long pauseDuration = 16 * 60 * 60000; // 16 hours
+const unsigned long executionDuration = 6 * 60 *60000; // 6 hour activity
+const unsigned long pauseDuration = 6 * 60 * 60000; // 6 hour pause
 unsigned long startTime;
 unsigned long currentTime;
 
 void setup() {
-  delay(10000);++
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(100);
