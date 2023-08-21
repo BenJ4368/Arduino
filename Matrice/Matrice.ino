5631c28206124f05e14d252c34650e0111f28488
Benjamin Gaertner <bgaertne@k0r4p8.42mulhouse.fr>
2023-08-21 10:25:29 +0200

    Arduino update

--- Matrice/Matrice.ino
+++ Matrice/Matrice.ino
@@ -2,57 +2,61 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 #ifndef PSTR
-  #define PSTR
+#define PSTR
 #endif
 
 #define PIN 4
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
-  NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
-  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-  NEO_GRB            + NEO_KHZ800);
+											   NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
+												   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+											   NEO_GRB + NEO_KHZ800);
 int x = matrix.width();
 int y = 0;
 
-const char *textToPrint = "Info: RDV hebdo mercredi 14h14 en cantina        SevTech: Rejoignez discord.gg/gjtGVZEWV6        ";
+const char *textToPrint = "Info: rdv hebdo mercredi 14h14 en cantina        Minecraft: Rejoignez-nous discord.gg/gjtGVZEWV6        Bonne Piscine";
 const int scrollSpeed = 30;
 int textWidth;
 
-const unsigned long executionDuration = 6 * 60 *60000;
+const unsigned long executionDuration = 6 * 60 * 60000;
 const unsigned long pauseDuration = 500;
 unsigned long startTime;
 unsigned long currentTime;
 
-void setup() {
-  matrix.begin();
-  matrix.setTextWrap(false);
-  matrix.setBrightness(100);
-  matrix.setTextColor(matrix.Color(0, 100, 255));
-  startTime = millis();
+void setup()
+{
+	matrix.begin();
+	matrix.setTextWrap(false);
+	matrix.setBrightness(100);
+	matrix.setTextColor(matrix.Color(255, 100, 0));
+	startTime = millis();
 }
 
-void loop() {
-    currentTime = millis();
+void loop()
+{
+	currentTime = millis();
 
-    if (currentTime - startTime <= executionDuration) {
-      if (x >= -textWidth) {
-        matrix.fillScreen(0);
-        matrix.setCursor(x, y);
-        matrix.print(textToPrint);
-        if (textWidth == 0)
-          textWidth = matrix.getCursorX();
-        x--;
-        matrix.show();
-        delay(scrollSpeed);
-      }
-      else
-        x = matrix.width();
-    }
-    else {
-      matrix.fillScreen(0);
-      matrix.show();
-      delay(pauseDuration);
-      startTime = millis();
-    }
+	if (currentTime - startTime <= executionDuration)
+	{
+		if (x >= -textWidth)
+		{
+			matrix.fillScreen(0);
+			matrix.setCursor(x, y);
+			matrix.print(textToPrint);
+			if (textWidth == 0)
+				textWidth = matrix.getCursorX();
+			x--;
+			matrix.show();
+			delay(scrollSpeed);
+		}
+		else
+			x = matrix.width();
+	}
+	else
+	{
+		matrix.fillScreen(0);
+		matrix.show();
+		delay(pauseDuration);
+		startTime = millis();
+	}
 }
-
